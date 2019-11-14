fb2ca0575e45b3bbd0e9aaedc6e195d677c9c6de
Sarah <sarahhargreaves10@gmail.com>
2019-11-14 18:38:56 -0500

    Initial commit

---
+++ src/main/java/com/newBankApi/newBankApi/NewBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.newBankApi.newBankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewBankApiApplication.class, args);
+	}
+
+}
