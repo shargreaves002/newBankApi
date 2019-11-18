2690d7f167783d1a2d1f8024e1a280700b470861
Sarah <sarahhargreaves10@gmail.com>
2019-11-18 16:09:51 -0500

    fixed generation strategies

--- src/main/java/com/newBankApi/newBankApi/models/Customer.java
+++ src/main/java/com/newBankApi/newBankApi/models/Customer.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class Customer {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String first_name;
